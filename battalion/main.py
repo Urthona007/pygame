0fbaf23f7c02128b7b375d746398f96d878999f3
Bill Blank <bubblank@gmail.com>
2022-04-11 23:39:15 -0400

    more thread and phase development.  Units now successfully walk all 6 directions.

--- battalion/main.py
+++ battalion/main.py
@@ -1,7 +1,9 @@
 """ Battalion Main """
-import pygame #pylint: disable=E0401
+import pygame
+from hexl import get_hex_coords_from_direction #pylint: disable=E0401
 import unit
 from hexl import draw_hexs
+from hexl import directions
 from threading import Thread
 from time import sleep
 from random import randrange
@@ -15,6 +17,22 @@ def reset_phases(game_dict):
     for i in range(len(game_dict["game_phases"])):
         game_dict["game_phases"][i] = (game_dict["game_phases"][i][0], False)
 
+def evaluate_combat(active_phase):
+    return
+
+def execute_phase(game_dict, active_phase):
+    if "combat" in active_phase:
+        evaluate_combat(active_phase)
+    else:
+        for player in game_dict["players"]:
+            for battalion in player.battalion:
+                if battalion.name in active_phase:
+                    for unit in battalion.units:
+                        newx, newy = get_hex_coords_from_direction(directions[game_dict["game_turn"]%6], unit.x, unit.y, game_dict)
+                        unit.x = newx
+                        unit.y = newy
+                        game_dict["update_screen"] = True
+
 def next_phase(game_dict):
     candidate_phases = []
     phase_list = game_dict["game_phases"]
@@ -23,20 +41,22 @@ def next_phase(game_dict):
             candidate_phases.append(phase)
     active_phase = candidate_phases[randrange(len(candidate_phases))]
     print(f"Executing {active_phase[0]}")
+    execute_phase(game_dict, active_phase[0])
     for i in range(len(phase_list)):
         if phase_list[i][0] == active_phase[0]:
             phase_list[i] = (active_phase[0], True)
     sleep(1)
 
 def play_game_threaded_function(game_dict, max_turns):
-    while game_dict["game_turn"] < max_turns:
+    while game_dict["game_turn"] < max_turns and game_dict["game_running"]:
         turn = game_dict["game_turn"]
         print(f"Turn {turn}")
-        while active_phases(game_dict):
+        while game_dict["game_running"] and active_phases(game_dict):
             next_phase(game_dict)
         reset_phases(game_dict)
         game_dict["game_turn"] += 1
-    print("GAME OVER: MAX TURNS {max_turns} reached.")
+    if game_dict["game_turn"] == max_turns:
+        print(f"\nGAME OVER: MAX TURNS {max_turns} reached.")
     game_dict["game_running"] = False
 
 class Battalion():
@@ -74,23 +94,31 @@ pygame.display.set_caption(game_dict['name']) # NOTE: this is not working.
 
 game_dict["game_running"] = True
 game_screen = pygame.display.set_mode((game_dict['display_width'], game_dict['display_height']))
-update_screen = True
+game_dict["update_screen"] = True
 
-if __name__ == "__main__":
-    thread = Thread(target = play_game_threaded_function, args = (game_dict, 10))
-    thread.start()
-#    thread.join()
-#    print("thread finished...exiting")
 
+
+first_time = True
+gamemaster_thread = None
 while game_dict["game_running"]:
     for event in pygame.event.get():
         # print(event)
         if event.type == pygame.QUIT:
             game_dict["game_running"] = False
-    if update_screen:
+    if game_dict["update_screen"]:
         game_screen.fill(game_dict['bkg_color'])
         draw_map(game_screen, game_dict)
         unit.draw_units(game_screen, game_dict)
         pygame.display.update()
-        update_screen = False
+        game_dict["update_screen"] = False
+        if first_time:
+            first_time = False
+            if __name__ == "__main__":
+                sleep(1)
+                gamemaster_thread = Thread(target = play_game_threaded_function, args = (game_dict, 10))
+                gamemaster_thread.start()
+                #    thread.join()
+                #    print("thread finished...exiting")
+if gamemaster_thread:
+    gamemaster_thread.join()
 pygame.quit()
