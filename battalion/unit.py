43fab84b40a5c782e239ccf6ab19ccd06f1f7151
Bill Blank <bubblank@gmail.com>
2022-04-14 17:53:18 -0400

    Added unit status.  Don't draw non-active unit.

--- battalion/unit.py
+++ battalion/unit.py
@@ -8,16 +8,19 @@ def draw_units(screen, game_dict):
     for player in game_dict["players"]:
         for battalion in player.battalion:
             for this_unit in battalion.units:
-                x_offset, y_offset = get_hex_offset(this_unit.x, this_unit.y, game_dict)
-                draw.rect(screen, player_unit_color[player.idx], \
-                    (x_offset + game_dict['unit_x_offset'], y_offset + game_dict['unit_y_offset'], \
-                    game_dict['unit_width'], game_dict['unit_width']))
+                if this_unit.status == "active":
+                    x_offset, y_offset = get_hex_offset(this_unit.x, this_unit.y, game_dict)
+                    draw.rect(screen, player_unit_color[player.idx], \
+                        (x_offset + game_dict['unit_x_offset'], y_offset + game_dict['unit_y_offset'], \
+                        game_dict['unit_width'], game_dict['unit_width']))
 
 class Unit():
     """ Basic game piece. """
-    def __init__(self, type, strength, x, y, player):
+    def __init__(self, name, type, strength, x, y, player):
         self.type = type
+        self.name = name
         self.strength = strength
         self.x = x
         self.y = y
         self.player = player # warning, chance of info in 2 places.
+        self.status = "active"
